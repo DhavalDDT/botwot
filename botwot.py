c69df91be0c9b5725ce5af7a15ac300b68f1b11a
rascul <root@rascul.io>
2014-04-30 19:31:13 -0400

    bunches of changes for betterness

--- botwot.py
+++ botwot.py
@@ -13,17 +13,17 @@ import urllib2
 import ConfigParser
 import random
 import re
+import anydbm
 
+from oyoyo import helpers
 from oyoyo.client import IRCClient
 from oyoyo.cmdhandler import DefaultCommandHandler
-from oyoyo import helpers
 
 import psycopg2
+import psycopg2.extras
 
 config = ConfigParser.ConfigParser()
 config.read(os.path.expanduser('~/.botwot.conf'))
-
-party_mode = False
 
 def connect_callback(bot):
 	nickpass = config.get('botwot', 'nickpass')
@@ -38,48 +38,36 @@ def num(self, chan):
 		f.close()
 		helpers.msg(self.client, chan, "There are currently %s players on the game." % (match.group(1),))
 	except:
-		helpers.msg(self.client, chan, "Is the mud down?")
+		helpers.msg(self.client, chan, "It looks like the mud may be down.")
 
-def time(self, chan):
-	try:
-		f = urllib2.urlopen('http://wotmud.org/num.php')
-		match = re.search('.*Last updated: (\w* \d{2}, \d{4}) @ (\d{2}:\d{2}) EST, \d{2}:\d{2} GMT.*', f.read())
-		f.close()
-		helpers.msg(self.client, chan, "Current server time (within 5 minutes) is %s EST on %s" % (match.group(2), match.group(1)))
-	except:
-		helpers.msg(self.client, chan, "Something is broken?")
-
-def reboot(self, chan):
-	try:
-		f = urllib2.urlopen('http://wotmud.org/num.php')
-		match = re.search('.*Since the last reboot \( (\d{1,2}) hours, (\d{1,2}) minutes ago \):.*', f.read())
-		f.close()
-		helpers.msg(self.client, chan, "Last reboot was %s hours, %s minutes ago." % (match.group(1), match.group(2)))
-	except:
-		helpers.msg(self.client, chan, "Reboot yourself")
-
-def wiki_search(self, chan, query):
-	try:
-		f = urllib2.urlopen('http://wotmud.wikia.com/api/v1/Search/List?query=%s&limit=1' % (urllib.quote_plus(query,)))
-		parsed_json = json.loads(f.read())
-		f.close()
-		helpers.msg(self.client, chan, parsed_json['items'][0]['url'])
-	except:
-		helpers.msg(self.client, chan, "couldn't find %s" % (query,))
-
-def smob_search(self, chan, query):
-	db = psycopg2.connect(config.get('botwot', 'postgres'))
-	cur = db.cursor()
-	q = "select name, smobid from smob where name ilike %s or shortname ilike %s limit 1"
-	search = '%' + query + '%'
-	cur.execute(q, (search, search))
-	if cur.rowcount == 1:
-		result = cur.fetchone()
-		message = "%s  http://smobdb.herokuapp.com/smobs/%s" % (result[0], result[1])
-		helpers.msg(self.client, chan, message)
+def wiki(self, chan, data):
+	if data:
+		try:
+			f = urllib2.urlopen('http://wotmud.wikia.com/api/v1/Search/List?query=%s&limit=1' % (urllib.quote_plus(data,)))
+			parsed_json = json.loads(f.read())
+			f.close()
+			helpers.msg(self.client, chan, parsed_json['items'][0]['url'])
+		except:
+			helpers.msg(self.client, chan, "Couldn't find %s" % (data,))
 	else:
-		helpers.msg(self.client, chan, "couldn't find %s" % (query,))
-	db.close()
+		helpers.msg(self.client, chan, "Wiki: http://wotmud.wikia.com")
+
+def smob(self, chan, data):
+	if data:
+		db = psycopg2.connect(config.get('botwot', 'postgres'))
+		cur = db.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+		q = "select name, smobid from smob where name ilike %s or shortname ilike %s limit 1"
+		search = '%' + str(data) + '%'
+		cur.execute(q, (search, search))
+		if cur.rowcount >= 1:
+			result = cur.fetchone()
+			message = "%s  http://smobdb.herokuapp.com/smobs/%s" % (result[0], result[1])
+			helpers.msg(self.client, chan, message)
+		else:
+			helpers.msg(self.client, chan, "Couldn't find %s" % (data,))
+		db.close()
+	else:
+		helpers.msg(self.client, chan, "Smob Database: http://smobdb.herokuapp.com")
 
 def insult(self, chan, nick):
 	lines = open('insults').read().splitlines()
@@ -89,54 +77,84 @@ def compliment(self, chan, nick):
 	lines = open('compliments').read().splitlines()
 	helpers.msg(self.client, chan, "%s: %s" % (nick, random.choice(lines)))
 
-def enable_party(self, chan):
-	global party_mode
-	if (party_mode):
-		helpers.msg(self.client, chan, "you're late to the party")
+def karma(self, chan, opnick, nick, action):
+	if nick == opnick:
+		helpers.msg(self.client, chan, "You can't mess with your own karma.")
 	else:
-		party_mode = True
-		helpers.msg(self.client, chan, "party time!")
+		db = anydbm.open(config.get('botwot', 'karma_db'), 'c')
+		
+		karma = 0
+		if nick in db:
+			karma = int(db[nick])
+		
+		if action == '++':
+			karma += 1
+			helpers.msg(self.client, chan, "%s has gained karma." % nick)
+		elif action == '--':
+			karma -= 1
+			helpers.msg(self.client, chan, "%s has lost karma." % nick)
+		
+		db[nick] = str(karma)
+		
+		db.close()
 
-def disable_party(self, chan):
-	global party_mode
-	if (party_mode):
-		party_mode = False
-		helpers.msg(self.client, chan, "party pooper")
-	else:
-		helpers.msg(self.client, chan, "what party?")
+def battle(self, chan, data):
+	m = re.match(r'^(\w+) vs (\w+)$', data.strip())
+	if m:
+		db = anydbm.open(config.get('botwot', 'karma_db'), 'c')
+		
+		nick1 = m.group(1)
+		nick2 = m.group(2)
+		
+		nick1_karma = 0
+		nick2_karma = 0
+		
+		if nick1 in db:
+			nick1_karma = int(db[nick1])
+		if nick2 in db:
+			nick2_karma = int(db[nick2])
+		
+		if nick1_karma > nick2_karma:
+			helpers.msg(self.client, chan, "%s beats %s" % (nick1, nick2))
+		elif nick2_karma > nick1_karma:
+			helpers.msg(self.client, chan, "%s beats %s" % (nick2, nick1))
+		elif nick1_karma == nick2_karma:
+			helpers.msg(self.client, chan, "No clear winner between %s and %s" % (nick1, nick2))
+		
+		db.close()
+
 
 class BotHandler(DefaultCommandHandler):
 	def privmsg(self, nick, chan, msg):
-		if (msg.startswith("botwot ")):
-			command = msg.replace("botwot ", "", 1).rstrip()
+		msg = msg.strip()
+		nick = nick[:nick.find('!')]
+		
+		m = re.match(r'^botwot\:? (\w+)( (.*)|)$', msg)
+		if m:
+			command = m.group(1)
+			data = m.group(3)
 			
-			if (command == "enable party"):
-				enable_party(self, chan)
-			elif (command == "disable party"):
-				disable_party(self, chan)
-			elif (command == "are you there?"):
-				helpers.msg(self.client, chan, "i'm here")
-			elif (command == "open sores"):
+			if command == "num":
+				num(self, chan)
+			elif command == "wiki":
+				wiki(self, chan, data)
+			elif command == "help":
 				source = config.get('botwot', 'source')
 				helpers.msg(self.client, chan, source)
-			elif (command == "wiki"):
-				helpers.msg(self.client, chan, "http://wotmud.wikia.com")
-			elif (command.startswith("wiki ")):
-				wiki_search(self, chan, command.replace("wiki ", "", 1))
-			elif (command.startswith("smob ")):
-				smob_search(self, chan, command.replace("smob ", "", 1))
-			elif (command.startswith("insult ")):
-				insult(self, chan, command.replace("insult ", "", 1))
-			elif (command.startswith("compliment ")):
-				compliment(self, chan, command.replace("compliment ", "", 1))
-			elif (command == "stats"):
-				helpers.msg(self.client, chan, "http://wotmad.herokuapp.com/stats  http://rascul.github.io/wotmad-stats")
-			elif (command == "num"):
-				num(self, chan)
-			elif (command == "time"):
-				time(self, chan)
-			elif (command == "reboot"):
-				reboot(self, chan)
+			elif command == "smob":
+				smob(self, chan, data)
+			elif command == "insult":
+				insult(self, chan, data)
+			elif command == "compliment":
+				compliment(self, chan, data)
+			elif command == "stats":
+				helpers.msg(self.client, chan, "Stats: http://wotmad.herokuapp.com/stats  http://rascul.github.io/wotmad-stats")
+			elif command == "battle":
+				battle(self, chan, data)
+		
+		m = re.match(r'^(\w+)(\+\+|\-\-)$', msg)
+		if m:
+			karma(self, chan, nick, m.group(1), m.group(2))
 
 def main():
 	loglevel = config.get('botwot', 'loglevel')
