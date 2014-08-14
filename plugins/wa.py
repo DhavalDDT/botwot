b579884554e8de97ea058bcb74995d0347304187
rascul <root@rascul.io>
2014-08-14 09:25:18 -0400

    add wolfram alpha plugin

---
+++ plugins/wa.py
@@ -0,0 +1,25 @@
+import wolframalpha
+
+from pyaib.plugins import keyword
+
+@keyword('wa')
+def keyword_wolframalpha(context, msg, trigger, args, kargs):
+	""" look up wolfram alpha """
+	
+	# figure out first who to target and what the query is
+	target_user = ""
+	query = ""
+	if len(args) >= 3 and args[-2] == "|":
+		target_user = args[-1]
+		query = " ".join(args[:-2])
+	else:
+		target_user = msg.sender
+		query = " ".join(args)
+	
+	wa = wolframalpha.Client(context.config.plugin.wa.appid)
+	res = wa.query(query)
+	if len(res.pods) > 1:
+		msg.reply("%s: %s" % (target_user, res.pods[1].text))
+	else:
+		msg.reply("%s: no results")
+
