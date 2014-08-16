2a7e37a078a22f23f27aec0b6c72585a9d6abb51
rascul <root@rascul.io>
2014-08-16 11:20:26 -0400

    add insults plugin

--- plugins/insults.py
+++ plugins/insults.py
@@ -10,8 +10,8 @@ def keyword_insult(context, msg, trigger, args, kargs):
 	# determine target user
 	target_user = " ".join(args) or msg.sender
 	
-	# grab a randomly generated insult from insultgenerator.org
-	page = requests.get("http://www.insultgenerator.org")
+	# grab a randomly generated insult from randominsults.net
+	page = requests.get("http://randominsults.net")
 	soup = BeautifulSoup(page.text)
-	msg.reply("%s: %s" % (target_user, soup.td.string))
+	msg.reply("%s: %s" % (target_user, soup.td.strong.i.string))
 	
