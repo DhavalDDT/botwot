c8f61878dd4e857ce209e7d6fd7586a96bdf87c6
rascul <root@rascul.io>
2014-08-16 12:44:21 -0400

    fix beer stuff and add beerparse script to create /tmp/beers.shelve

--- plugins/beer.py
+++ plugins/beer.py
@@ -1,10 +1,10 @@
+import shelve
 import random
-random.seed()
-
-import requests
-from bs4 import BeautifulSoup
+from collections import namedtuple
 
 from pyaib.plugins import keyword
+
+random.seed()
 
 @keyword("beer")
 def keyword_beer(context, msg, trigger, args, kargs):
@@ -13,17 +13,12 @@ def keyword_beer(context, msg, trigger, args, kargs):
 	# determine target user
 	target_user = " ".join(args) or msg.sender
 	
-	# grab the top 250 beers from beeradvocate.com
-	page = requests.get("http://tmp.rascul.io/beers.html")
-	soup = BeautifulSoup(page.text)
-	beer = soup.table.contents[random.randint(4, len(soup.table.contents))].contents[1].span
+	beers = shelve.open("/tmp/beers.shelve")
+	num_beers = len(beers.keys()) - 1
 	
-	beer_name = beer.contents[0].text
-	beer_url = "http://www.beeradvocate.com%s" % beer.contents[0].get("href")
-	beer_brewer = beer.contents[2].text
-	beer_abv = beer.contents[5]
+	beer = beers[str(random.randint(0, num_beers))]
 	
 	context.PRIVMSG(msg.channel or msg.sender, 
-		"\x01ACTION hands %s a %s%s from %s (%s)\x01" % (target_user, beer_name, beer_abv, beer_brewer, beer_url))
+		"\x01ACTION hands %s a %s%s from %s (%s)\x01" % (target_user, beer["name"], beer["abv"], beer["brewer"], beer["url"]))
 	
 	
