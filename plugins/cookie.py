45085f626e028ab33e14025612903e69132675ca
rascul <root@rascul.io>
2014-12-28 15:01:38 -0500

    make .scancookie command active, check admin

--- plugins/cookie.py
+++ plugins/cookie.py
@@ -15,6 +15,26 @@ class Cookie(object):
 		print "%s cookies are in the jar." % len(self.cookies)
 	
 	
+	def scancookies(self):
+		""" Parse the list of cookies and add them to the db """
+		print "scanning cookies..."
+		counter = 0
+		
+		page = requests.get("http://en.wikipedia.org/wiki/List_of_cookies")
+		soup = BeautifulSoup(page.text)
+		
+		# grab each table row, drop the header
+		cookie_cells = [tr.td for tr in soup.table.find_all("tr")][1:]
+		# grab the cookie name from each row, some have links and some don't
+		self.cookies = [getattr(c.contents[0], "text", None) or getattr(c, "text", None) for c in cookie_cells]
+		
+		# Fill the database
+		for c in self.cookies:
+			print "adding %s to the cookie jar" % c
+			item = self.db.get(c)
+			item.commit()		
+	
+	
 	@keyword("cookie")
 	def keyword_cookie(self, context, msg, trigger, args, kargs):
 		""" hand out some cookies """
@@ -36,21 +56,6 @@ class Cookie(object):
 	def keyword_scancookies(self, context, msg, trigger, args, kargs):
 		""" load the cookie list into the database """
 		
-		print "scanning cookies..."
-		counter = 0
-		
-		page = requests.get("http://en.wikipedia.org/wiki/List_of_cookies")
-		soup = BeautifulSoup(page.text)
-		
-		# grab each table row, drop the header
-		cookie_cells = [tr.td for tr in soup.table.find_all("tr")][1:]
-		# grab the cookie name from each row, some have links and some don't
-		self.cookies = [getattr(c.contents[0], "text", None) or getattr(c, "text", None) for c in cookie_cells]
-		
-		# Fill the database
-		for c in self.cookies:
-			print "adding %s to the cookie jar" % c
-			item = self.db.get(c)
-			item.commit()
-		
+		if msg.sender == context.config.IRC.admin:
+			self.scancookies()
 
