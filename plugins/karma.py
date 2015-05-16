7d8ad1cd04ac53dfe0179e069156c980d4d3a188
rascul <git@rascul.io>
2015-05-15 22:58:11 -0400

    add k and karma

--- plugins/karma.py
+++ plugins/karma.py
@@ -22,13 +22,32 @@ import time
 from pyaib.plugins import keyword, observe, plugin_class
 from pyaib.db import db_driver
 
-
 @plugin_class
 @plugin_class.requires('db')
 class Factoids(object):
 	def __init__(self, context, config):
 		self.context = context
 		self.db = context.db.get("karma")
+		self.damage_types = [
+			["blast", "blasts"],
+			["cleave", "cleaves"],
+			["crush", "crushes"],
+			["hack", "hacks"],
+			["hit", "hits"],
+			["lance", "lances"],
+			["pierce", "pierces"],
+			["pound", "pounds"],
+			["scythe", "scythes"],
+			["shoot", "shoots"],
+			["slash", "slashes"],
+			["slice", "slices"],
+			["smite", "smites"],
+			["stab", "stabs"],
+			["sting", "stings"],
+			["strike", "strikes"],
+			["whip", "whips"]]
+		
+		
 		random.seed()
 	
 	
@@ -37,9 +56,71 @@ class Factoids(object):
 		return "".join(ch for ch in s if ch not in set(string.punctuation)).lower()
 	
 	
+	def hit(self, attacker, defender):
+		""" 
+		hit somebody
+		return true if attacker hits defender, else false
+		"""
+		
+		total = attacker["abs_karma"] + attacker["abs_karma"] / 2 + defender["abs_karma"] + defender["abs_karma"] / 2
+		total = total + 10 if total < 20 else total
+		
+		res = random.randint(1, total)
+		
+		if res <= attacker["abs_karma"]:
+			return True
+		elif total - defender["abs_karma"] < res <= total:
+			return False
+		else:
+			if random.choice([attacker, defender]) == attacker:
+				return True
+			else:
+				return False
+		
+	
+	def fight(self, msg, attacker, defender):
+		status = 0
+		
+		if self.hit(attacker, defender):
+			status += 1
+			msg.reply("%s %s %s" % (attacker["name"], random.choice(self.damage_types)[1], defender["name"]))
+		else:
+			status -= 1
+			msg.reply("%s fails to %s %s" % (attacker["name"], random.choice(self.damage_types[0]), defender["name"]))
+		
+		if self.hit(defender, attacker):
+			status -= 1
+			msg.reply("%s %s %s" % (defender["name"], random.choice(self.damage_types)[1], attacker["name"]))
+		else:
+			status += 1
+			msg.reply("%s fails to %s %s" % (defender["name"], random.choice(self.damage_types[0]), attacker["name"]))
+		
+		return status
+	
+	
 	@keyword('k')
 	def keyword_kill(self, context, msg, trigger, args, kargs):
-		pass
+		if len(args) != 1:
+			return
+		
+		attacker = {}
+		defender = {}
+		
+		attacker["name"] = msg.sender
+		defender["name"] = args[0]
+		
+		for i in [attacker, defender]:
+			i["karma"] = int(self.db.get("%s/karma" % i["name"]).value) or 0
+			i["abs_karma"] = abs(i["karma"])		
+		
+		status = self.fight(msg, attacker, defender)
+	
+	
+	@keyword('karma')
+	def keyword_karma(self, context, msg, trigger, args, kargs):
+		""" tell you karmas """
+		karma = self.db.get("%s/karma" % msg.sender) or 0
+		context.PRIVMSG(msg.sender, "You have %s karmas." % karma.value)
 	
 	
 	@observe("IRC_MSG_PRIVMSG")
