f67c9b9edd9696cfd3f5a196925ebc0af035609f
rascul <git@rascul.io>
2015-05-21 15:29:30 -0400

    move 8 ball sayings out of config file

--- plugins/chance.py
+++ plugins/chance.py
@@ -25,6 +25,28 @@ class Chance(object):
 	def __init__(self, context, config):
 		self.context = context
 		random.seed()
+		
+		self.sayings = [
+			"It is certain",
+			"It is decidedly so",
+			"Without a doubt",
+			"Yes, definitely",
+			"You may rely on it",
+			"As I see it, yes",
+			"Most likely",
+			"Outlook good",
+			"Yes",
+			"Signs point to yes",
+			"Reply hazy, try again",
+			"Ask again later",
+			"Better not tell you now",
+			"Cannot predict now",
+			"Concentrate and ask again",
+			"That's a definite possibility",
+			"Don't count on it",
+			"My reply is no",
+			"Outlook not so good",
+			"Very doubtful"]
 	
 	
 	@keyword('8')
@@ -34,9 +56,9 @@ class Chance(object):
 		"""
 		
 		if len(args) >= 2 and args[-2] == "|":
-			msg.reply("%s: %s" % (args[-1], random.choice(self.context.config.plugin.chance.sayings)))
+			msg.reply("%s: %s" % (args[-1], random.choice(self.sayings)))
 		else:
-			msg.reply("%s" % random.choice(self.context.config.plugin.chance.sayings))
+			msg.reply("%s" % random.choice(self.sayings))
 	
 	
 	@keyword('choose')
